102ad618cb8f5eb152d00e805857521d35b11b18
Atta Muhammad <attaforge@gmail.com>
2026-06-22 22:53:19 +0500

    Add three-lane movement system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,11 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float forwardSpeed = 10f;
 
+    [SerializeField] private float laneDistance = 3f;
+    [SerializeField] private float laneChangeSpeed = 10f;
+
+    private int currentLane = 1;
+
     private CharacterController controller;
 
     private void Awake()
@@ -14,8 +19,37 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 movement = Vector3.forward * forwardSpeed;
+        HandleLaneInput();
+
+        Vector3 targetPosition = transform.position;
+        targetPosition.x = (currentLane - 1) * laneDistance;
+
+        float horizontalMovement =
+            (targetPosition.x - transform.position.x) * laneChangeSpeed;
+
+        Vector3 movement = new Vector3(
+            horizontalMovement,
+            0f,
+            forwardSpeed
+        );
 
         controller.Move(movement * Time.deltaTime);
+    }
+
+    private void HandleLaneInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A) ||
+            Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentLane--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) ||
+            Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentLane++;
+        }
+
+        currentLane = Mathf.Clamp(currentLane, 0, 2);
     }
 }
